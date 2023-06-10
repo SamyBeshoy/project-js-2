947c9798e4cb0c1c0c60541201c938ee2e585c62
beshoy-englizy <120126754+beshoy-englizy@users.noreply.github.com>
2023-06-10 07:08:36 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+document.title = "Project 1";
+document.body.style.cssText = "font-family: Arial, Helvetica, sans-serif;font-size: 14px; height: 100vh; background-image: linear-gradient(211deg, rgb(138, 209, 255), rgb(60, 201, 157), rgb(152, 255, 234));";
+let title = document.createElement("h3"); 
+title.innerHTML = "Click Her To Get Random Number";
+title.style.fontSize = "1.3em"
+document.body.appendChild(title);
+let parent = document.createElement("div");
+parent.style.cssText = "display: flex; align-items: center; flex-direction: column; align-items: center; font-size: 1.2em;";
+let btn = document.createElement("button");
+btn.id = "btn"
+btn.innerHTML = "Click"
+parent.appendChild(btn)
+let element = document.createElement("div");
+let text = document.createTextNode(Math.random());
+element.appendChild(text);
+btn.onclick = function () {
+    parent.appendChild(element);
+};
+document.body.appendChild(parent);
+let footer = document.createElement("div");
+footer.innerHTML = "Copyright 2023 © by beshoy samy";
+footer.style.cssText = "position: absolute ;bottom: 0; margin-left: 1em;";
+document.body.appendChild(footer);
